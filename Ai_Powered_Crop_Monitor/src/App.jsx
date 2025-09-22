8f3637305fd134b6d5ed2d2939b2a15e0765899b
aakash0101200 <aakashkumargupta.cse27@jecrc.ac.in>
2025-09-22 12:40:48 +0530

    sky

--- Ai_Powered_Crop_Monitor/src/App.jsx
+++ Ai_Powered_Crop_Monitor/src/App.jsx
@@ -8,14 +8,15 @@ function App() {
 
   return (
     <>
-    <HeroSection/>
-    <div className="container my-0 mx-auto mt-auto p-4 text-center">
-        <h2 className="text-2xl font-bold mt-10">Welcome to AI Powered Crop Monitor 🌱</h2>
+      <HeroSection />
+      <section className="bg-white text-black py-10 text-center">
+        <h2 className="text-2xl font-bold">Welcome to AI Powered Crop Monitor 🌱</h2>
         <p className="mt-2 text-gray-700">
           This application helps farmers monitor their crops using advanced AI technologies.
         </p>
-      </div>
-    </>
+      </section>
+     </>
+    
   )
 }
 
