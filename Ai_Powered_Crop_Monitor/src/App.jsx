eedacf9eafce18250cadb8e444104f14af1050ba
Aditya Mishra <aditya.mishra9257@gmail.com>
2025-09-26 14:02:21 +0530

    mishra here

--- Ai_Powered_Crop_Monitor/src/App.jsx
+++ Ai_Powered_Crop_Monitor/src/App.jsx
@@ -111,5 +111,4 @@ function App() {
     </Router>
   );
 }
-
 export default App;
--- Ai_Powered_Crop_Monitor/src/main.jsx
+++ Ai_Powered_Crop_Monitor/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { HeroUIProvider } from "@heroui/react";
 import App from "./App";
 import "./index.css";
